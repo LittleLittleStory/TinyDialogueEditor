2d46894bd4b9d5fa9a1a7d24f33a6cfe1101f5cc
LittleLittleStory <36630033+LittleLittleStory@users.noreply.github.com>
2020-08-13 17:59:00 +0800

    初始化提交

---
+++ TinyDialogEditor/Assets/NewNodeGraph.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateAssetMenu(fileName = "String Graph", menuName = "xNode Examples/String Graph")]
+public class NewNodeGraph : NodeGraph { 
+	
+}
